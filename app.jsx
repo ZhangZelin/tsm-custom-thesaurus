d525db0c4a92ea6fd26c73be91b2c66439373bee
Chris <christonechen@gmail.com>
2018-02-27 15:27:13 -0500

    Moved page swapping to Base class. Page swapping now functional. Radio buttons to select thesaurus added. Warning for lack of functionality

--- app.jsx
+++ app.jsx
@@ -2,16 +2,26 @@
 class BaseThesaurus extends React.Component {
 	constructor(props){
 		super(props);
-		this.state = {value: ''};
+		this.state = {value: '', source: 'sourceStandard'};
 
 		this.handleChange = this.handleChange.bind(this);
 		this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleSourceChange = this.handleSourceChange.bind(this);
 	}
 	handleChange(event){
 		this.setState({value: event.target.value});
 	}
 	handleSubmit(event){
-		alert('synonms for ' + this.state.value);
+		event.preventDefault();
+		if(this.state.source==="sourceStandard"){
+			alert('synonms for: ' + this.state.value + '\nFunctionality not implemented');
+		}
+		else{
+			alert('synonms for: ' + this.state.value + ' from custom thesaurus\n Functionality not implemented');
+		}
+	}
+	handleSourceChange(event){
+		this.setState({source: event.target.value});
 	}
   	render() {
 	    return (
@@ -22,46 +32,31 @@ class BaseThesaurus extends React.Component {
 		        		Word:
 		        		<input type="text" value={this.state.value} onChange={this.handleChange}/>
 	        		</label>
+	        		<div className="radio">
+		        		<label>
+		        			<input type="radio" value="sourceStandard" 
+		        				checked={this.state.source==="sourceStandard"} 
+		        				onChange={this.handleSourceChange}/>
+
+		        			Standard Thesaurus
+		        		</label>
+		        		<label>
+		        			<input type="radio" value="sourceCustom" 
+		        				checked={this.state.source==="sourceCustom"} 
+		        				onChange={this.handleSourceChange}/>
+		        			Custom Thesaurus
+		        		</label>
+		        	</div>
 	        		<input type="submit" value="Submit" />
 		        </form>
+		        
 		        <ul className="dicResList">
 		        </ul>
       		</div>
-	    )
+	    );
 	}
 }
-class Bottom extends React.Component {
-	constructor(props){
-		super(props);
-		this.state = {job: 1};
-		this.handleRgClick = this.handleRgClick.bind(this);
-		this.handleCClick = this.handleCClick.bind(this);
 
-	}
-	handleRgClick(){
-		this.setState({job : 1});
-		console.log('Reg');
-		ReactDOM.render(<BaseThesaurus />, document.getElementById('root'));
-	}
-	handleCClick(){
-		this.setState({job : 2});
-		console.log('Cust');
-		ReactDOM.render(<AddCThesaurus />, document.getElementById('root'));
-	}
-	render(){
-		return(
-			<div className="change">
-		      	<form onSubmit={this.handleCClick} >
-		      		<input type="submit" value="Custom Th." />
-	      		</form>
-      			<form onSubmit={this.handleRgClick} >
-		      		<input type="submit" value="Reg Th." />
-	      		</form>
-	      		
-			</div>
-		)
-	}
-}
 class AddCThesaurus extends React.Component {
 	constructor(props){
 		super(props);
@@ -79,6 +74,7 @@ class AddCThesaurus extends React.Component {
 		this.setState({value2: event.target.value});
 	}
 	handleSubmit(event){
+		event.preventDefault();
 		alert('Added ' + this.state.value2 + 'as synonm for ' + this.state.value);
 	}
   	render() {
@@ -96,14 +92,57 @@ class AddCThesaurus extends React.Component {
         		</label>
         		<input type="submit" value="Submit" />
 	        </form>
-	        <ul class="dicResList">
+	        <ul className="dicResList">
 	        </ul>
 	      </div>
-	    )
+	    );
 	}
+}
+class Base extends React.Component{
+	constructor(props){
+		super(props);
+		this.state = {job: 1};
+		this.handleRgClick = this.handleRgClick.bind(this);
+		this.handleCClick = this.handleCClick.bind(this);
+
+	}
+	handleRgClick(){
+		this.setState({job: 1});
+		console.log('Reg');
+	}
+	handleCClick(){
+		this.setState({job: 2});
+		console.log('Cust');
+	}
+
+	render() {
+		const job = this.state.job;
+		let page = null;
+		if(job == 1){
+			page = <BaseThesaurus />;
+		}
+		else{
+			page = <AddCThesaurus />;
+
+		}
+		return(
+			
+			<div>
+				{page}
+		      	<button onClick={this.handleCClick} >
+		      		Custom Th.
+	      		</button>
+      			<button onClick={this.handleRgClick} >
+		      		Reg Th.
+	      		</button>
+	      		
+			</div>
+		);
+	}
+
 }
 // This asks ReactDOM to add the component SimpleWidget
 // Note the JSX for specifiying a React component
 
-ReactDOM.render(<BaseThesaurus />, document.getElementById('root'));
-ReactDOM.render(<Bottom />, document.getElementById('bottom'));
+ReactDOM.render(<Base />, document.getElementById('root'));
+//ReactDOM.render(<Bottom />, document.getElementById('bottom'));
