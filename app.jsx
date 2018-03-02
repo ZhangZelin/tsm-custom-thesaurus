52bf4484e87ebd263dd21905de2cc93deaef8f69
root <root@My-Desktop.localdomain>
2018-03-01 21:24:23 -0500

    used bootstrap to make the web application look nicer, added a home page

--- app.jsx
+++ app.jsx
@@ -1,142 +1,213 @@
 
 class BaseThesaurus extends React.Component {
-	constructor(props){
+	constructor(props) {
 		super(props);
-		this.state = {value: '', source: 'sourceStandard'};
+		this.state = { value: '', source: 'sourceStandard' };
 
 		this.handleChange = this.handleChange.bind(this);
 		this.handleSubmit = this.handleSubmit.bind(this);
 		this.handleSourceChange = this.handleSourceChange.bind(this);
 	}
-	handleChange(event){
-		this.setState({value: event.target.value});
+	handleChange(event) {
+		this.setState({ value: event.target.value });
 	}
-	handleSubmit(event){
+	handleSubmit(event) {
 		event.preventDefault();
-		if(this.state.source==="sourceStandard"){
-			alert('synonms for: ' + this.state.value + '\nFunctionality not implemented');
+		if (this.state.source === "sourceStandard") {
+			//alert('synonms for: ' + this.state.value + '\nFunctionality not implemented');
+			var REG = document.getElementById("returnReg");
+			REG.append('<li>An element</li>');
 		}
-		else{
+		else {
 			alert('synonms for: ' + this.state.value + ' from custom thesaurus\n Functionality not implemented');
 		}
 	}
-	handleSourceChange(event){
-		this.setState({source: event.target.value});
+	handleSourceChange(event) {
+		this.setState({ source: event.target.value });
 	}
-  	render() {
-	    return (
-      		<div className="bt">
-		        <h3>Thesaurus</h3>
-		        <form onSubmit={this.handleSubmit}>
-		        	<label>
-		        		Word:
-		        		<input type="text" value={this.state.value} onChange={this.handleChange}/>
-	        		</label>
-	        		<div className="radio">
-		        		<label>
-		        			<input type="radio" value="sourceStandard" 
-		        				checked={this.state.source==="sourceStandard"} 
-		        				onChange={this.handleSourceChange}/>
+	render() {
+		return (
+			<div className="bt">
+				<h3>Thesaurus</h3>
+				<form onSubmit={this.handleSubmit}>
+					<label>
+						Word:
+		        		<input type="text" value={this.state.value} onChange={this.handleChange} />
+					</label>
+					<div className="radio">
+						<label>
+							<input type="radio" value="sourceStandard"
+								checked={this.state.source === "sourceStandard"}
+								onChange={this.handleSourceChange} />
 
-		        			Standard Thesaurus
+							Standard Thesaurus
 		        		</label>
-		        		<label>
-		        			<input type="radio" value="sourceCustom" 
-		        				checked={this.state.source==="sourceCustom"} 
-		        				onChange={this.handleSourceChange}/>
-		        			Custom Thesaurus
+						<label>
+							<input type="radio" value="sourceCustom"
+								checked={this.state.source === "sourceCustom"}
+								onChange={this.handleSourceChange} />
+							Custom Thesaurus
 		        		</label>
-		        	</div>
-	        		<input type="submit" value="Submit" />
-		        </form>
-		        
-		        <ul className="dicResList">
-		        </ul>
-      		</div>
-	    );
+					</div>
+					<input className="btn btn-primary" type="submit" value="Search" />
+				</form>
+
+				<ul id="returnReg" className="dicResList">
+				</ul>
+			</div>
+		);
 	}
 }
 
 class AddCThesaurus extends React.Component {
-	constructor(props){
+	constructor(props) {
 		super(props);
-		this.state = {value: '', value2: ''};
+		this.state = { value: '', value2: '' };
 
 		this.handleChange = this.handleChange.bind(this);
 		this.handleChange2 = this.handleChange2.bind(this);
 
 		this.handleSubmit = this.handleSubmit.bind(this);
 	}
-	handleChange(event){
-		this.setState({value: event.target.value});
+	handleChange(event) {
+		this.setState({ value: event.target.value });
 	}
-	handleChange2(event){
-		this.setState({value2: event.target.value});
+	handleChange2(event) {
+		this.setState({ value2: event.target.value });
 	}
-	handleSubmit(event){
+	handleSubmit(event) {
 		event.preventDefault();
 		alert('Added: ' + this.state.value2 + '\nSynonm of: ' + this.state.value + '\nFunctionality not implemented');
 	}
-  	render() {
-	    return (
-	      <div className="bt">
-	        <h3>Add to Custom Thesaurus</h3>
-	        <form onSubmit={this.handleSubmit}>
-	        	<label>
-	        		Word:
-	        		<input type="text" value={this.state.value} onChange={this.handleChange}/>
-        		</label>
-        		<label>
-	        		Synonm:
-	        		<input type="text" value={this.state.value2} onChange={this.handleChange2}/>
-        		</label>
-        		<input type="submit" value="Submit" />
-	        </form>
-	        <ul className="dicResList">
-	        </ul>
-	      </div>
-	    );
+	render() {
+		return (
+			<div className="bt">
+				<h3>Add to Custom Thesaurus</h3>
+				<form onSubmit={this.handleSubmit}>
+					<label>
+						Word:
+	        		<input type="text" value={this.state.value} onChange={this.handleChange} />
+					</label>
+					<label>
+						Synonm:
+	        		<input type="text" value={this.state.value2} onChange={this.handleChange2} />
+					</label>
+					<input className="btn btn-primary" type="submit" value="Submit" />
+				</form>
+				<ul className="dicResList">
+				</ul>
+			</div>
+		);
 	}
 }
-class Base extends React.Component{
-	constructor(props){
+
+class WelcomePage extends React.Component {
+	constructor(props) {
 		super(props);
-		this.state = {job: 1};
+	}
+	render() {
+		return (
+			<div>
+				<h1 className="cover-heading">Welcome to TSM Custom Thesaurus.</h1>
+							<p className="lead">TSM Custom Thesaurus is a personalized, customizable thesaurus that builds upon the thesaurus from the Oxford English Dictionary API.</p>
+							<p className="lead">
+								<a onClick={this.handleRgClick} className="btn btn-lg btn-default">How does it work?</a>
+							</p>
+			</div>
+		);
+	}
+}
+
+class Base extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { job: 0 };
+		this.handleHClick = this.handleHClick.bind(this);
 		this.handleRgClick = this.handleRgClick.bind(this);
 		this.handleCClick = this.handleCClick.bind(this);
 
 	}
-	handleRgClick(){
-		this.setState({job: 1});
+	handleHClick() {
+		this.setState({ job: 0 });
+		var NAME = document.getElementById("home");
+		NAME.className="active";
+		var CUS = document.getElementById("cus");
+		CUS.classList.remove("active");
+		var THES = document.getElementById("thes");
+		THES.classList.remove("active");
+		console.log('Home');
+	}
+	handleRgClick() {
+		this.setState({ job: 1 });
+		var NAME = document.getElementById("thes");
+		NAME.className="active";
+		var HOME = document.getElementById("home");
+		HOME.classList.remove("active");
+		var CUS = document.getElementById("cus");
+		CUS.classList.remove("active");
 		console.log('Reg');
 	}
-	handleCClick(){
-		this.setState({job: 2});
+	handleCClick() {
+		this.setState({ job: 2 });
+		var NAME = document.getElementById("cus");
+		NAME.className="active";
+		var HOME = document.getElementById("home");
+		HOME.classList.remove("active");
+		var THES = document.getElementById("thes");
+		THES.classList.remove("active");
 		console.log('Cust');
 	}
 
 	render() {
 		const job = this.state.job;
 		let page = null;
-		if(job == 1){
+		if (job == 0) {
+			page = <WelcomePage />;
+		}
+		else if (job == 1) {
 			page = <BaseThesaurus />;
 		}
-		else{
+		else {
 			page = <AddCThesaurus />;
 
 		}
-		return(
-			
-			<div>
-				{page}
-		      	<button onClick={this.handleCClick} >
-		      		Custom Th.
-	      		</button>
-      			<button onClick={this.handleRgClick} >
-		      		Reg Th.
-	      		</button>
-	      		
+		return (
+			<div className="site-wrapper">
+				<div className="site-wrapper-inner">
+
+					<div className="container">
+
+						<div className="masthead clearfix">
+							<div className="container inner">
+								<h3 className="masthead-brand">TSM Custom Thesaurus</h3>
+								<nav>
+									<ul className="nav masthead-nav">
+										<li id="home" className="active"><a href="#" onClick={this.handleHClick}>Home</a></li>
+										<li id="cus"><a href="#" onClick={this.handleCClick}>Edit Custom Thesaurus</a></li>
+										<li id="thes"><a href="#" onClick={this.handleRgClick}>Use Thesaurus</a></li>
+									</ul>
+								</nav>
+							</div>
+						</div>
+
+						<div className="inner cover">
+							{page}
+						</div>
+
+					</div>
+
+				</div>
+
 			</div>
+			//<div>
+			//	{page}
+			// 	<button type class="btn btn-primary" onClick={this.handleCClick} >
+			//  		Edit Custom Thesaurus.
+			//	</button>
+			//	<button type class="btn btn-primary" onClick={this.handleRgClick} >
+			// 		Use Thesaurus.
+			//	</button>
+			//</div>
 		);
 	}
 
