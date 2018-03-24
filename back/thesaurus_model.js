94e5bf72f80bbc4567c957959a6593900b264d0d
Chris <christonechen@gmail.com>
2018-03-24 14:08:57 -0400

    mongoose schema

---
+++ back/thesaurus_model.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+// Doc for Mongoose Schemas: http://mongoosejs.com/docs/guide
+const Schema = mongoose.Schema;
+
+const wordSchema = new Schema({
+
+	word: {	type: String,
+		required: true},
+	synonms: [String]
+
+
+},{collection: 'thesaurus'}
+
+);
+
+mongoose.connect('mongodb://localhost/thesaurusdb', (error) => {
+  if (error) console.log(error);
+
+  console.log('Database connection successful');
+
+});
+
+// Doc for Mongoose Models: http://mongoosejs.com/docs/models
+
+// Use the schema to create a model
+module.exports = mongoose.model('Word', wordSchema);
