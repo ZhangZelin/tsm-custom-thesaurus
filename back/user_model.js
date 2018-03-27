27eb4636febbf3250215d13b01fbd0e938b1bfe8
Chris <christonechen@gmail.com>
2018-03-27 15:25:24 -0400

    user model

---
+++ back/user_model.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+// Doc for Mongoose Schemas: http://mongoosejs.com/docs/guide
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+
+	username: {type: String,
+		required: true},
+	password: {type: String,
+		required: true},
+
+
+
+},{collection: 'thesaurus'}
+
+);
+
+//mongoose.connect('mongodb://localhost/thesaurusdb', (error) => {
+//  if (error) console.log(error);
+
+//  console.log('Database connection successful');
+
+//});
+
+// Doc for Mongoose Models: http://mongoosejs.com/docs/models
+
+// Use the schema to create a model
+module.exports = mongoose.model('Word', userSchema);
